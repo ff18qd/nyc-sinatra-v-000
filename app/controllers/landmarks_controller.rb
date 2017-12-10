0bec2f918eaa590b8ad36cda9c565ad10927d504
Emma Feng <ff18qd@gmail.com>
2017-12-10 17:18:42 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,11 +9,12 @@ class LandmarksController < ApplicationController
   end
 
   get "/landmarks/new" do
-    binding.pry
+
     erb :"landmarks/new"
   end
 
   post "/landmarks" do
+      binding.pry
     @landmark = Landmark.create(name: params["landmark_name"], year_completed: params["landmark_year_completed"])
     redirect to "/landmarks/#{@landmark.id}"
   end
