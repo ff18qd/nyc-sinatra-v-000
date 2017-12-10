8379c392e79029dbf901bad2ff151a35a19b7676
Emma Feng <ff18qd@gmail.com>
2017-12-10 04:41:55 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,9 +14,7 @@ class FiguresController < Sinatra::Base
   end
 
   post '/figures' do
-    binding.pry
     @figure = Figure.create(name: params[:figure_name])
-
     if params[:new_title] !=""
       @newtitle = Title.create(name: params[:new_title])
       @figure.titles << @newtitle
@@ -27,10 +25,34 @@ class FiguresController < Sinatra::Base
       @figure.landmarks << @newlandmark
     end
 
+    if params["figure"]["title_ids"]
+      @figure.titles << Title.find_by_id(params["figure"]["title_ids"][0])
+    end
+
+    if params["figure"]["landmark_ids"]
+      @figure.landmarks << Landmark.find_by_id(params["figure"]["landmark_ids"][0])
+    end
     @figure.save
-    redirect to "figures/#{@figure.id}"
+    redirect to "/figures/#{@figure.id}"
   end
 
+  get "/figures/:id" do
+    # binding.pry
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/show"
+  end
 
+  get "/figures/:id/edit" do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/edit"
+  end
+
+  patch "/figures/:id" do
+    binding.pry
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params["figure_name"]
+
+    redirect to "/figures/#{@figure.id}"
+  end
 
 end
