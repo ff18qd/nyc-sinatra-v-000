f7f64b40e99d48b66cc6dc84bbf69884e912430a
Emma Feng <ff18qd@gmail.com>
2017-12-10 15:57:32 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
 
+  set :views, Proc.new { File.join(root, "../views/") }
+  register Sinatra::Twitter::Bootstrap::Assets
+
+  get "/landmarks" do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get "/landmarks/new" do
+    erb :"landmarks/new"
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmark/show"
+  end
+
 end
